6f997e44368f79481d6e158e46e482516666f91d
ylylylimt <110519247+ylylylimt@users.noreply.github.com>
2023-10-10 20:01:22 +0200

    Create opt_queue.cpp

---
+++ opt_queue.cpp
@@ -0,0 +1,57 @@
+//memory-efficient: used same vector data, no need in the helper vector
+//faster: constant multipliers are smaller in for loop
+#include <iostream>
+#include <vector>
+
+class Queue {
+public:
+    Queue() : data(BLOCK_SIZE), nextFree(0), nextFull(0), inUse(0) {}
+
+    void enqueue(int value) {
+        if (inUse == data.size()) {
+            enlargeQueue();
+        }
+        data.at(nextFree) = value;
+        nextFree = (nextFree + 1) % data.size();
+        inUse++;
+    }
+
+    int dequeue() {
+        if (isEmpty()) {
+            throw std::runtime_error("Queue is empty");
+        }
+        int result = data.at(nextFull);
+        nextFull = (nextFull + 1) % data.size();
+        inUse--;
+        return result;
+    }
+
+    bool isEmpty() const {
+        return inUse == 0;
+    }
+
+    void print() {
+        for (int i = nextFull, count = 0; count < inUse; i = (i + 1) % data.size(), count++) {
+            std::cout << data.at(i) << " ";
+        }
+    }
+
+private:
+    const int BLOCK_SIZE = 10;
+    std::vector<int> data;
+    int nextFree, nextFull, inUse;
+
+    void enlargeQueue() {
+        int newSize = data.size() + BLOCK_SIZE;
+        std::vector<int> newData(newSize);
+
+        for (int i = 0; i < inUse; i++) {
+            newData.at(i) = data.at(nextFull);
+            nextFull = (nextFull + 1) % data.size();
+        }
+
+        data = newData;
+        nextFull = 0;
+        nextFree = inUse;
+    }
+};
