2db916e12226a70a5556b4d60aae43ad79ac1696
Alua Rakhmankulova <110519247+ylylylimt@users.noreply.github.com>
2024-01-21 20:23:27 +0100

    Update opt_queue.cpp

--- opt_queue.cpp
+++ opt_queue.cpp
@@ -1,51 +1,23 @@
-//memory-efficient: used same vector data, no need in the helper vector
-//faster: constant multipliers are smaller in for loop
-#include <iostream>
-#include <vector>
+window.addEventListener('DOMContentLoaded', (event) => {
+    let form = document.querySelector("#add-item-form");
+    form.addEventListener("submit", submitHandler);
+});
+         
+function submitHandler(event) {
+    event.preventDefault(); 
+    var imageUrl = document.getElementById('poster').value;
+    var description = document.getElementById('description').value;
 
-class Queue {
-public:
-    Queue() : data(BLOCK_SIZE), nextFree(0), nextFull(0), inUse(0) {}
+    
+    let img = document.createElement('img');
+    img.src = imageUrl;
+    
+    let desc = document.createElement('p');
+    desc.innerText = description;
 
-    void enqueue(int value) {
-        if (inUse == data.size()) {
-            enlargeQueue();
-        }
-        data.at(nextFree) = value;
-        nextFree = (nextFree + 1) % data.size();
-        inUse++;
-    }
+    let container = document.querySelector('.gallery');  
+    container.appendChild(img);
+    container.appendChild(desc);
 
-    int dequeue() {
-        if (isEmpty()) {
-            throw std::runtime_error("Queue is empty");
-        }
-        int result = data.at(nextFull);
-        nextFull = (nextFull + 1) % data.size();
-        inUse--;
-        return result;
-    }
-
-    bool isEmpty() const {
-        return inUse == 0;
-    }
-
-private:
-    const int BLOCK_SIZE = 10;
-    std::vector<int> data;
-    int nextFree, nextFull, inUse;
-
-    void enlargeQueue() {
-        int newSize = data.size() + BLOCK_SIZE;
-        std::vector<int> newData(newSize);
-
-        for (int i = 0; i < inUse; i++) {
-            newData.at(i) = data.at(nextFull);
-            nextFull = (nextFull + 1) % data.size();
-        }
-
-        data = newData;
-        nextFull = 0;
-        nextFree = inUse;
-    }
-};
+   
+}
